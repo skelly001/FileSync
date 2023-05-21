1709593f8a296610bea688ef72ea470d1c556d3e
skelly001 <97435404+skelly001@users.noreply.github.com>
2023-05-21 15:04:44 -0700

    add pushShane

---
+++ R/pushShane.R
@@ -0,0 +1,51 @@
+#' Synchronize Folders
+#'
+#' This function synchronizes files between a source folder and a destination folder.
+#'
+#' @return None
+#' @export
+
+pushShane <- function() {
+    
+    sourceFolder <- r"(C:\Users\kell343\OneDrive - PNNL\Documents\dev\test_source)"
+    destinationFolder <- r"(\\pnl\projects\MSSHARE\Shane_Kelly_MSS\powershel_test_dest)"
+    
+    # Check if source folder is accessible
+    if (!dir.exists(sourceFolder)) {
+        stop("Source folder is not accessible!")
+    }
+    
+    # Check if destination folder is accessible
+    if (!dir.exists(destinationFolder)) {
+        stop("Destination folder is not accessible!")
+    }
+    
+    syncFiles <- function(sourcePath, destinationPath) {
+        file <- basename(sourcePath)
+        destination <- file.path(destinationPath, sub(sourceFolder, "", file))
+        
+        tryCatch({
+            if (file.exists(sourcePath)) {
+                file.copy(sourcePath, destination, overwrite = TRUE)
+                cat(paste(file, "- Synced\n"), file = "stdout", append = TRUE)
+            } else if (file.exists(destination)) {
+                file.remove(destination)
+                cat(paste(file, "- Deleted\n"), file = "stdout", append = TRUE)
+            }
+        }, error = function(e) {
+            cat(paste(file, "- Sync Failed\n"), file = "stdout", append = TRUE)
+        })
+    }
+    
+    syncFilesInFolder <- function(folderPath) {
+        files <- list.files(path = folderPath, recursive = TRUE, full.names = TRUE)
+        
+        for (file in files) {
+            syncFiles(file, destinationFolder)
+        }
+    }
+    
+    syncFilesInFolder(sourceFolder)
+    
+    cat("Folder synchronization completed.\n", file = "stdout")
+}
